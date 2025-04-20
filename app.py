2eb747efcc98b35eb898c500599d8489fbd39b87
rudzkimichal <michal.rudzki@yahoo.com>
2025-04-21 00:36:21 +0200

    url change

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 from client import client
 
 app = Flask(__name__)
+basePath = '/api/v1'
 
-@app.route('/')
+@app.route(basePath + '/all')
 def all():
     books = client.books.items.find()
     return str(list(books))
