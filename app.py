881e20d4a1228f1dfe2a38ce7df3cb17dadd4986
rudzkimichal <michal.rudzki@yahoo.com>
2025-06-04 21:57:32 +0200

    refactor

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+import json
 from flask import Flask
+from flask_cors import CORS
+
 from client import client
 
 app = Flask(__name__)
+CORS(app, origins=['http://localhost:3000'])
 basePath = '/api/v1'
 
 @app.route(basePath + '/all')
 def all():
     books = client.books.items.find()
-    return str(list(books))
+    return json.dumps([book for book in books], default=str)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
